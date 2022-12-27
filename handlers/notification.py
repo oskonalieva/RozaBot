e016d7492898be5806a132d1c74ad71b3101cd62
oskonalieva <oskonalievaroza316@icloud.com>
2022-12-27 16:32:29 +0600

    hw5

---
+++ handlers/notification.py
@@ -0,0 +1,26 @@
+import aioschedule
+from aiogram import types, Dispatcher
+from config import bot
+import asyncio
+
+
+async def get_id(message: types.Message):
+    global chat_id
+    chat_id = message.from_user.id
+    await message.answer("Хорошо!")
+
+
+async def go_to_library():
+    await bot.send_message(chat_id=chat_id, text="Сегодня надо в библиотеку!")
+
+
+async def scheduler():
+    aioschedule.every().saturday.at('11:00').do(go_to_library())
+    while True:
+        await aioschedule.run_pending()
+        await asyncio.sleep(2)
+
+
+def register_handlers_notification(dp: Dispatcher):
+    dp.register_message_handler(get_id,
+                                lambda word: "напомни" in word.text)
--- main.py
+++ main.py
@@ -1,20 +1,28 @@
 from config import dp
 from aiogram.utils import executor
-from handlers import client, callback, extra, fsmAdminMentor,admin
+import asyncio
+from handlers import client, callback, extra, fsmAdminMentor
+from handlers import client, callback, extra, fsmAdminMentor, admin, notification
 import logging
+
 from database.bot_db import sql_create
 from database import bot_db
 bot_db.register_handlrs_bot_db(dp)
 admin.register_handlers_admin(dp)
 fsmAdminMentor.register_handlers_fsm_admin_mentor(dp)
 callback.register_handler_callback(dp)
+notification.register_handlers_notification(dp)
+
 client.register_handler_client(dp)
 
 extra.register_handler_extra(dp)
-async def on_startup(_):
+async def  on_startup(_):
+    asyncio.create_task(notification.scheduler())
     sql_create()
 
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
+    executor.start_polling(dp, skip_updates=True)
     executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
+
